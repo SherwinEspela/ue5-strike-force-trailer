d498cf37ec3abb49794300b1a48ebf6b68cd84ae
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2025-02-21 09:34:35 -0800

    Adding Unreal project source

---
+++ StrikeForceTrailer/Source/StrikeForceTrailer/StrikeForceTrailer.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StrikeForceTrailer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StrikeForceTrailer, "StrikeForceTrailer" );
+
+DEFINE_LOG_CATEGORY(LogStrikeForceTrailer)
+ 
---
+++ StrikeForceTrailer/Source/StrikeForceTrailer/StrikeForceTrailerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StrikeForceTrailerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStrikeForceTrailerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AStrikeForceTrailerGameMode();
+};
+
+
+
